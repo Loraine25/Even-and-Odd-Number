c003b6970605c261bef82bace606e0ad851c6a51
unknown <anonuevoloraine6@gmail.com>
2023-04-26 22:41:35 +0800

    Greetings

---
+++ even_and_odd.py
@@ -0,0 +1,9 @@
+#Anonuevo, Loraine N. 
+#BSCpE 1-4
+#Even and Odd Text Files
+
+#Header
+print("\033[;33;1;3m\033[10m" * 65)
+print("\033[;33;1;3mHello! I'm Loraine, and I'll be your programmer for today!\033[0m".center(81))
+print("\033[;33;1;3mಠ\033[10m" * 65)
+
